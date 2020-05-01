627cea0707e3a73e740968cee4dc7410c292f6ba
Bent Brüggemann <mail@bent-brueggemann.de>
2020-05-01 16:58:26 +0200

    installation of laravel 7.9.2 + added uuid concern + created models + controllers

---
+++ app/Models/Dashboard.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Concerns\UsesUuid;
+use Illuminate\Database\Eloquent\Model;
+
+class Dashboard extends Model
+{
+    use UsesUuid;
+
+}
---
+++ app/Models/WebHook.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Concerns\UsesUuid;
+use Illuminate\Database\Eloquent\Model;
+
+class WebHook extends Model
+{
+    use UsesUuid;
+}
