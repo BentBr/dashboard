a9e5cdd7f96c6e17af68aa081c7fc9d4f696391e
Bent Brüggemann <mail@bent-brueggemann.de>
2020-05-03 22:39:13 +0200

    added reloading (shiddy whole-site reload) version

---
+++ resources/js/components/ReloadComponent.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+        <v-btn>
+            <span>reloading every 10 secs</span>
+        </v-btn>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ReloadComponent",
+
+        data () {
+            return {
+                active: true
+            }
+        },
+
+        methods: {
+            //making sure page is reloading frequently
+            autoUpdate: function () {
+                if (this.active === true) {
+                    setInterval((function () {
+                        //quick and dirty reload
+                        location.reload()
+
+                    }),10000);
+                }
+            }
+        },
+
+        //invoking of reloading function after component rendered
+        created() {
+            this.autoUpdate();
+        },
+    }
+</script>
